a31ae964d2504a84289920c52893c5df6e9e6b5b
soyer <soyer@irl.hu>
2017-12-19 00:44:01 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ cd frontend &&
 npm install &&
 npm run dist &&
 cd .. &&
-go get &&
+go get github.com/jteeuwen/go-bindata/... &&
+go get github.com/elazarl/go-bindata-assetfs/... &&
+go get ./... &&
 go generate ./api &&
 go build
