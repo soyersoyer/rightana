199f5aad6cdc2a44f90ab6579434de6e399e668a
soyer <soyer@irl.hu>
2018-05-12 22:18:52 +0200

    comments for cipobolt

--- db/cipobolt/cipobolt.go
+++ db/cipobolt/cipobolt.go
@@ -7,15 +7,22 @@ import (
 	"github.com/boltdb/bolt"
 )
 
+// EncFun is an encoder function type for encoding the types
 type EncFun func(interface{}) ([]byte, error)
+
+// DecFun is a decoder function type for decoding the types
 type DecFun func([]byte, interface{}) error
+
+// BucFun is a function type for getting the bucket names
 type BucFun func(interface{}) []byte
 
+// The error types
 var (
 	ErrKeyNotExists = errors.New("key not exists")
 	ErrKeyExists    = errors.New("key exists")
 )
 
+// DB is the struct for the DB
 type DB struct {
 	bolt   *bolt.DB
 	encode EncFun
@@ -23,6 +30,7 @@ type DB struct {
 	bucket BucFun
 }
 
+// Open opens the database, sets the encoder/decoder functions
 func Open(b *bolt.DB, ef EncFun, df DecFun, bf BucFun) *DB {
 	return &DB{
 		bolt:   b,
@@ -32,64 +40,77 @@ func Open(b *bolt.DB, ef EncFun, df DecFun, bf BucFun) *DB {
 	}
 }
 
+// Bolt returns the underlying bolt instance
 func (db *DB) Bolt() *bolt.DB {
 	return db.bolt
 }
 
+// Get gets the value based on key
 func (db *DB) Get(key interface{}, value interface{}) error {
 	return db.bolt.View(func(tx *bolt.Tx) error {
 		return db.GetTx(tx, key, value)
 	})
 }
 
+// CountPrefix counts the values which belongs to a prefix key
 func (db *DB) CountPrefix(prefix interface{}, value interface{}, count *int) error {
 	return db.bolt.View(func(tx *bolt.Tx) error {
 		return db.CountPrefixTx(tx, prefix, value, count)
 	})
 }
 
+// Iterate iterates over the bucket and run fn on every element
+// the key and the value stores the actual keys/values
 func (db *DB) Iterate(key interface{}, value interface{}, fn func() error) error {
 	return db.bolt.View(func(tx *bolt.Tx) error {
 		return db.IterateTx(tx, key, value, fn)
 	})
 }
 
+// IteratePrefix iterates the elements with prefix key over the bucket and run fn on every element
+// the key and the value stores the actual keys/values
 func (db *DB) IteratePrefix(key interface{}, value interface{}, prefix interface{}, fn func() error) error {
 	return db.bolt.View(func(tx *bolt.Tx) error {
 		return db.IteratePrefixTx(tx, key, value, prefix, fn)
 	})
 }
 
+// Update updates an element
 func (db *DB) Update(key interface{}, value interface{}) error {
 	return db.bolt.Update(func(tx *bolt.Tx) error {
 		return db.UpdateTx(tx, key, value)
 	})
 }
 
+// Insert inserts an element
 func (db *DB) Insert(key interface{}, value interface{}) error {
 	return db.bolt.Update(func(tx *bolt.Tx) error {
 		return db.InsertTx(tx, key, value)
 	})
 }
 
+// Upsert upserts an element
 func (db *DB) Upsert(key interface{}, value interface{}) error {
 	return db.bolt.Update(func(tx *bolt.Tx) error {
 		return db.UpsertTx(tx, key, value)
 	})
 }
 
+// Delete deletes an element
 func (db *DB) Delete(key interface{}, value interface{}) error {
 	return db.bolt.Update(func(tx *bolt.Tx) error {
 		return db.DeleteTx(tx, key, value)
 	})
 }
 
+// DeletePrefix deletes elements with prefix
 func (db *DB) DeletePrefix(prefix interface{}, value interface{}) error {
 	return db.bolt.Update(func(tx *bolt.Tx) error {
 		return db.DeleteTx(tx, prefix, value)
 	})
 }
 
+// GetTx gets an element in a transaction
 func (db *DB) GetTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	bb := db.bucket(value)
 	b := tx.Bucket(bb)
@@ -110,6 +131,7 @@ func (db *DB) GetTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	return db.decode(data, value)
 }
 
+// CountPrefixTx counts elements with prefix in a transaction
 func (db *DB) CountPrefixTx(tx *bolt.Tx, prefix interface{}, value interface{}, count *int) error {
 	*count = 0
 	bb := db.bucket(value)
@@ -130,6 +152,8 @@ func (db *DB) CountPrefixTx(tx *bolt.Tx, prefix interface{}, value interface{}, 
 	return nil
 }
 
+// IterateTx iterates over the bucket in a transaction
+// calls fn in every element, stores the actual key/values in key/value
 func (db *DB) IterateTx(tx *bolt.Tx, key interface{}, value interface{}, fn func() error) error {
 	bb := db.bucket(value)
 	b := tx.Bucket(bb)
@@ -151,6 +175,8 @@ func (db *DB) IterateTx(tx *bolt.Tx, key interface{}, value interface{}, fn func
 	return nil
 }
 
+// IteratePrefixTx iterates elements with prefix over the bucket in a transaction
+// calls fn in every element, stores the actual key/values in key/value
 func (db *DB) IteratePrefixTx(tx *bolt.Tx, key interface{}, value interface{}, prefix interface{}, fn func() error) error {
 	bb := db.bucket(value)
 	b := tx.Bucket(bb)
@@ -177,6 +203,7 @@ func (db *DB) IteratePrefixTx(tx *bolt.Tx, key interface{}, value interface{}, p
 	return nil
 }
 
+// UpdateTx updates an element in a transaction
 func (db *DB) UpdateTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	bb, kb, vb, err := db.getBytes(key, value)
 	if err != nil {
@@ -193,6 +220,7 @@ func (db *DB) UpdateTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	return b.Put(kb, vb)
 }
 
+// InsertTx inserts an element in a transaction
 func (db *DB) InsertTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	bb, kb, vb, err := db.getBytes(key, value)
 	if err != nil {
@@ -209,6 +237,7 @@ func (db *DB) InsertTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	return b.Put(kb, vb)
 }
 
+// UpsertTx upserts an element in a transaction
 func (db *DB) UpsertTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	bb, kb, vb, err := db.getBytes(key, value)
 	if err != nil {
@@ -220,6 +249,7 @@ func (db *DB) UpsertTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	return b.Put(kb, vb)
 }
 
+// DeleteTx deletes an element in a transaction
 func (db *DB) DeleteTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	kb, err := db.encode(key)
 	if err != nil {
@@ -239,6 +269,7 @@ func (db *DB) DeleteTx(tx *bolt.Tx, key interface{}, value interface{}) error {
 	return b.Delete(kb)
 }
 
+// DeletePrefixTx deletes elements with prefix in a transaction
 func (db *DB) DeletePrefixTx(tx *bolt.Tx, prefix interface{}, value interface{}) error {
 	pb, err := db.encode(prefix)
 	if err != nil {
